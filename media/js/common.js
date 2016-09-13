6355df871e27b63a7f038b3d9789db73b42054c6
Alexander Perevezentsev <apcom@live.ru>
2016-09-13 22:54:24 +0300

    + Карта перемещается к маркеру + Добавлена анимация маркера + Когда пользователь вводит страну, все предыдущие маркеры удаляются * В списке городов увеличены отступы + Добавлена кнопка "Подробнее"

--- media/js/common.js
+++ media/js/common.js
@@ -1,17 +1,17 @@
 var map = null;
+var markers = [];
 
 function initMap() {
 	var options = {
 		scrollwheel: false,
 		zoom: 8,
-		disableDefaultUI: false,
+		disableDefaultUI: true,
+		zoomControl: true,
 		center: {lat: 58.6011769, lng: 49.6525304},
 	}
 
 	map = new google.maps.Map(document.getElementById('map'), options);
 }
-
-// google.maps.event.addDomListener(window, "load", initialize);
 
 $(function() {
 	var loading = '<div class="loading-layout"></div>'
@@ -23,31 +23,32 @@ $(function() {
 	$('body').on('click', '.sights-list .sights-list__item', function() {
 		var current = $(this);
 		var array_id = parseInt($(this).data('arrayId'));
-		console.log(current);
 		var title = current.find('.sights-list__item__title').text();
 		var place = current.find('.sights-list__item__content').text();
 
-		$('.sights-list').remove();
-		$('#backButton').show();
-		$('#refreshButton').hide();
-
-		console.log(title, place);
+		$('.card-content').html('');
 
 		var source = $('#sightDetailTemplate').html();
 		var template = Handlebars.compile(source);
 		var html = template(sights_list[array_id]);
-		$('.card-content').append(html);
+		$('.card-content').html(html);
 
 		var coords = sights_list[array_id].coords;
 		var gCoords = {
 			lat: coords[0], 
 			lng: coords[1]
 		}
-		var marker = new google.maps.Marker({
-	        map: map,
-	        position: gCoords,
-	        title: 'Hello World!'
-        });
+
+		map.setCenter(gCoords);
+		setTimeout(function() {
+			var marker = new google.maps.Marker({
+		        map: map,
+		        position: gCoords,
+		        animation: google.maps.Animation.DROP,
+		        title: sights_list[array_id].title
+	        });
+	        markers.append(marker);
+		}, 1000);		
 
 		var carousel = new Carousel($('body .carousel'));
 	});
@@ -63,7 +64,9 @@ $(function() {
 			}, function (response) {  
 				console.log(response);
             	if (response != "ok") {
-            		$('.search_field').show();
+            		if (response.length > 0) {
+            			$('.search_field').show();            			
+            		}
 
             		var source = $('#searchFieldTemplate').html();
 					var template = Handlebars.compile(source);
@@ -92,6 +95,10 @@ $(function() {
 				sights_list = response;
         	}
         });
+
+        for (var i = 0; i < markers.length; i++) {
+		    markers[i].setMap(null);
+		}
 	}
 
 	$('body').on('click', '[data-country-id]', function() {
